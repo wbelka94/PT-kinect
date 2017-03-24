08fee3cb195e9a54730572c9a96ed4e420da1b13
wbelka94 <wbelka94@gmail.com>
2017-03-24 10:12:02 +0100

    Wykrywanie Kinecta

--- FB Kinect Painter/MainWindow.xaml.cs
+++ FB Kinect Painter/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*************************************************************************************/
+/*                                  OUR REFERENCES                                   */
+/*************************************************************************************/
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.Controls;
+using Microsoft.Kinect.Toolkit.Interaction;
+/*************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +22,37 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace FB_Kinect_Painter
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const String APPNAME = "FB Kinect Painter 1.0 pre-alpha";
+        private const String ERR_NOKINECT = "Nie znaleziono sensora Kinect. Aby kontynuować podłącz urządzenie!";
+        private KinectSensorChooser sensorChooser;
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args)
+        {
+            if (args.NewSensor == null)
+            {
+                MessageBox.Show(ERR_NOKINECT, APPNAME, MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            this.sensorChooser = new KinectSensorChooser();
+            this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+            this.sensorChooserUi.KinectSensorChooser = this.sensorChooser;
+            this.sensorChooser.Start();
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
         }
     }
 }
